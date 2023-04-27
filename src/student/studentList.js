c2994e8c7481270de08be407918a8fc53c033c44
ramchjob <ramchjob@gmail.com>
2023-04-27 14:04:02 -0500

    Added table styling

--- src/student/studentList.js
+++ src/student/studentList.js
@@ -1,37 +1,40 @@
 import { useEffect, useState } from "react";
-
-function StudentList () {
+import './student.css'
+function StudentList() {
     const [students, setStudents] = useState([]);
-    
+
     useEffect(() => {
         fetch("http://localhost:8080/student")
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-            setStudents(data)
-        })
-    })
-    
-    
-    return ( 
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setStudents(data)
+            })
+    },[])
+
+
+    return (
         <div>
-        {students.length > 0 && (
-          <table>
-            <tr>
-              <th>id</th>
-              <th>name</th>
-            </tr>
-           
-            {students.map(student => (
-                <tr>
-                  <td>  {student.id}</td>
-                  <td>  {student.name}</td>
-              </tr>
-            ))}
-          </table>
-        )}
-      </div>
+            {students.length > 0 && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {students.map(student => (
+                            <tr>
+                                <td>  {student.id} </td>
+                                <td>  {student.name} </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
     );
 
 }
