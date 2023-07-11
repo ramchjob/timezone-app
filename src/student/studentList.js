da66a1966a620a43165b5ed8f3e3ed7ade5d3ef9
ramchjob <ramchjob@gmail.com>
2023-07-11 16:31:52 -0500

    Updated components

--- src/student/studentList.js
+++ src/student/studentList.js
@@ -5,31 +5,47 @@ import AddStudent from "./addStudent";
 function StudentList() {
     const [students, setStudents] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:8080/student")
+    const fetchUserData = () => {
+    
+    
+        fetch("http://localhost:8080/student" )
             .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                setStudents(data)
-            })
-    },[])
+                return response.json();
+
+            }).then((data) => {
+                setStudents(data);
+              })
+              .catch((error) => {
+                
+              })
+             
+    }
+    useEffect(() => {
+        fetchUserData()
+    }, [])
     function deleteStudent(student) {
         fetch("http://localhost:8080/student/" + student.id, {method: "DELETE"})
         .then(response => {
+            fetchUserData();
             return;
         })
         .catch(error => {console.log(error)})
     };
 
+    function editStudent(student) {
+        
+    };
+
     return (
         <div>
             {students.length > 0 && (
-                <table>
+                <table class="center">
                     <thead>
                         <tr>
                             <th>Id</th>
                             <th>Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,12 +53,15 @@ function StudentList() {
                             <tr>
                                 <td>  {student.id} </td>
                                 <td>  {student.name} </td>
+                                <td>  {student.email} </td>
+                                <td>  <button onClick={() => editStudent(student)}>Edit</button> </td>
                                 <td>  <button onClick={() => deleteStudent(student)}>Delete</button> </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             )}
+            <br/>
             <AddStudent />
         </div>
     );
